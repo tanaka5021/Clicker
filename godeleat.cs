5ccfff4307683d4f4c053301380155a6004d3c95
tanaka5021 <100393059+tanaka5021@users.noreply.github.com>
2022-08-15 11:50:47 +0900

    Add files via upload
    セーブデータ削除へ向かう

---
+++ godeleat.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class godeleat : MonoBehaviour
+{
+    public void GoDeletaButton()
+    {
+        SceneManager.LoadScene("deleat");
+
+    }
+}
