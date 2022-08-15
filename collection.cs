993c4f14a2775ce81689f243671989813a347264
tanaka5021 <100393059+tanaka5021@users.noreply.github.com>
2022-08-15 11:38:46 +0900

    Add files via upload
    ゲーム内で卵を購入した際にコレクション機能があるためその機能です

---
+++ collection.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class collection : MonoBehaviour
+{
+    public void GoCollectionButton()
+    {
+        SceneManager.LoadScene("collection");
+
+    }
+}
