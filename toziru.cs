dc29b61b78771c79629af7b56be626dd841c8536
tanaka5021 <100393059+tanaka5021@users.noreply.github.com>
2022-08-15 11:53:12 +0900

    Add files via upload
    遊び方を閉じるため

---
+++ toziru.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class toziru : MonoBehaviour
+{
+
+    public void BuckButton()
+    {
+        SceneManager.LoadScene("title");
+
+    }
+}
